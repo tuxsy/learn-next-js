bce18c6f9fdec82807a3d0df3e06e8330aed2427
Bru Anglés Robles <bangles@gmail.com>
2024-01-31 20:04:54 +0100

    my-app: routing fundamentals

---
+++ my-app/src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+    return (
+      <div>
+        <h1>404</h1>
+        <p>
+          This is a custom page to handle 404 errors.
+        </p>
+      </div>
+    )
+}
---
+++ my-app/src/app/test/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>This is a test</h1>
+      <p>
+        This is a page created inside a route segment (test/) to verify my understanding of routing in Next.js.
+      </p>
+    </div>
+  )
+}
