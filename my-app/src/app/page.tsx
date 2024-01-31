871ae6f6a5463efc9facb7d16952961e0f26a66a
Bru Anglés Robles <bangles@gmail.com>
2024-01-31 19:24:39 +0100

    my-app: finished installation

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>Hello, Next.js!</h1>
+      <p>
+        I&apos;ve finished with the Next.js setup. Now I can focus on writing my app.
+      </p>
+    </div>
+  )
+}
