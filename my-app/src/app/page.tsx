3b1c300d8f1bfd8ea7fda69751a0ac4955687cca
Bru Anglés Robles <bangles@gmail.com>
2024-02-01 18:25:34 +0100

    my-app: pages and layouts + linking and navigating

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,10 +1,5 @@
+import Link from 'next/link'
+ 
 export default function Page() {
-  return (
-    <div>
-      <h1>Hello, Next.js!</h1>
-      <p>
-        I&apos;ve finished with the Next.js setup. Now I can focus on writing my app.
-      </p>
-    </div>
-  )
+  return <Link href="/dashboard#settings">Dashboard</Link>
 }
---
+++ my-app/src/app/team/[id]/page.tsx
@@ -0,0 +1,21 @@
+import { redirect } from 'next/navigation'
+
+async function fetchTeam(id: string) {
+    const res = await fetch(`https://dummyjson.com/user/${id}`)
+    if (!res.ok) return undefined
+
+    return res.json()
+}
+
+export default async function Profile({ params }: { params: { id: string } }) {
+    const team = await fetchTeam(params.id);
+    if (!team) {
+        redirect('/login')
+    }
+
+    return (
+        <div>
+            {team && JSON.stringify(team)}
+        </div>
+    );
+}
